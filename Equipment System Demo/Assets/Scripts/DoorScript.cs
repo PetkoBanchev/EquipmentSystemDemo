09a0b3e581bf80947c3fe43d0e9a422f56605c57
P_Banchev <petkobanchev1122@gmail.com>
2023-02-28 19:07:53 +0100

    Opening/Closing doors
    Pressing the E key while the door tooltip is on the screen opens/closes the door

--- Equipment System Demo/Assets/Scripts/DoorScript.cs
+++ Equipment System Demo/Assets/Scripts/DoorScript.cs
@@ -7,6 +7,8 @@ public class DoorScript : MonoBehaviour, IInteractive
     [SerializeField] private string tooltipText = "Press E to open the door";
     [SerializeField] private ObjectType objectType = ObjectType.INTERACTIVE;
 
+    [SerializeField] private bool isDoorOpen = false;
+
     public string TooltipText { get { return tooltipText; } }
     public ObjectType ObjectType { get { return objectType; } }
     public Transform Transform { get { return transform; } }
@@ -25,6 +27,24 @@ public class DoorScript : MonoBehaviour, IInteractive
 
     public void TriggerInteraction()
     {
-        Debug.Log("Open Door");
+        ToggleDoor();
+    }
+
+    private void ToggleDoor()
+    {
+        if (!isDoorOpen) 
+        {
+            Debug.Log("Opened Door");
+            transform.parent.Rotate(0, -90, 0);
+            tooltipText = "Press E to close the door";
+        }
+        else
+        {
+            Debug.Log("Closed Door");
+            transform.parent.Rotate(0, 90, 0);
+            tooltipText = "Press E to open the door";
+        }
+
+        isDoorOpen= !isDoorOpen;
     }
 }
