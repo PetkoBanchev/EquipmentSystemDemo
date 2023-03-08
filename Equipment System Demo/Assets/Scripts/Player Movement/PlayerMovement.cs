cea82d4afa965939d4f827363c57f51a1f0d5805
P_Banchev <petkobanchev1122@gmail.com>
2023-03-08 13:52:51 +0100

    Added Jumping & gravity

--- Equipment System Demo/Assets/Scripts/Player Movement/PlayerMovement.cs
+++ Equipment System Demo/Assets/Scripts/Player Movement/PlayerMovement.cs
@@ -8,14 +8,34 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private CharacterController characterController;
     [SerializeField] private float speed = 5f;
 
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundMask;
+    [SerializeField] private float groundDistance = 0.4f;
+    [SerializeField] private float gravity = -9.2f;
+    [SerializeField] private float jumpHeight = 3f;
+    private Vector3 velocity;
+    private bool isGrounded;
+
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+            velocity.y = -2f;
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
 
         characterController.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        characterController.Move(velocity * Time.deltaTime);
     }
 }
