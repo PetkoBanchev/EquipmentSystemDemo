5e333663da62b694e60987dc0971f01c6c969f0f
P_Banchev <petkobanchev1122@gmail.com>
2023-03-10 16:17:59 +0100

    Added manual gunfire

--- Equipment System Demo/Assets/Scripts/Object Scripts/GunScript.cs
+++ Equipment System Demo/Assets/Scripts/Object Scripts/GunScript.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class GunScript : MonoBehaviour, IEquipable
 {
     [SerializeField] private EquipableType equipableType = EquipableType.HAND;
@@ -13,6 +14,26 @@ public class GunScript : MonoBehaviour, IEquipable
     private Vector3 localScale;
     private GroundCheck groundCheck;
 
+    [SerializeField] private int bulletCount = 5;
+    [SerializeField] private int maxBullets = 30;
+    private AudioSource gunSound;
+    [SerializeField] private AudioClip gunshot;
+    [SerializeField] private AudioClip emptyGun;
+
+    private Hand hand;
+
+    public int BulletCount
+    {
+        get { return bulletCount; }
+        set
+        {
+            bulletCount = value;
+            SetGunSound();
+            if (bulletCount > maxBullets)
+                bulletCount = maxBullets;
+        }
+    }
+
     public EquipableType EquipableType { get { return equipableType; } }
     public string TooltipText { get { return tooltipText; } }
     public ObjectType ObjectType { get { return objectType; } }
@@ -22,17 +43,22 @@ public class GunScript : MonoBehaviour, IEquipable
     {
         groundCheck = GetComponentInChildren<GroundCheck>();
         localScale = transform.localScale;
+        gunSound = GetComponent<AudioSource>();
     }
 
     public void OnEquip()
     {
         if(rigidbody != null)
             Destroy(rigidbody);
+        DetermineHand();
+        ManageEvent(SubMode.SUBSCRIBING);
+        SetGunSound();
     }
 
     public void OnUnequip()
     {
         transform.parent = null;
+        ManageEvent(SubMode.UNSUBSCRIBING);
         rigidbody = transform.AddComponent<Rigidbody>();
         StartCoroutine(Throw());
     }
@@ -50,5 +76,66 @@ public class GunScript : MonoBehaviour, IEquipable
         transform.localScale = localScale;
     }
 
+    private void FireGunManually()
+    {
+        if(bulletCount > 0)
+        {
+            bulletCount -= 1;
+        }
+        else
+        {
+            SetGunSound();
+        }
+        gunSound.Play();
+    }
 
+    private void SetGunSound()
+    {
+        if (bulletCount > 0)
+            gunSound.clip = gunshot;
+        else
+            gunSound.clip = emptyGun;
+    }
+
+    private void ManageEvent(SubMode mode)
+    {
+        if (mode == SubMode.SUBSCRIBING)
+        {
+            if (hand == Hand.LEFT)
+            {
+                InputManager.Instance.OnFire1 += FireGunManually;
+                Debug.Log("Sub left");
+            }
+            else
+            {
+                InputManager.Instance.OnFire2 += FireGunManually;
+                Debug.Log("Sub right");
+            }
+        }
+        else
+        {
+            if (hand == Hand.LEFT)
+            {
+                InputManager.Instance.OnFire1 -= FireGunManually;
+                EquipmentManager.Instance.UnsubscribeHand(hand);
+                Debug.Log("Unsub left");
+            }
+            else
+            {
+                InputManager.Instance.OnFire2 -= FireGunManually;
+                EquipmentManager.Instance.UnsubscribeHand(hand);
+                Debug.Log("Unsub right");
+            }
+        }
+    }
+
+    private void DetermineHand()
+    {
+        if (transform.parent.parent.name == "Left Hand")
+            hand = Hand.LEFT;
+        else
+            hand = Hand.RIGHT;
+
+        Debug.Log(hand);
+    }
 }
